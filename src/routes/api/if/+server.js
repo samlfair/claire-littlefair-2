f53d103c852fbf91f6d005250a7bc6561939d7bd
Sam Littlefair <sam.littlefair@prismic.io>
2022-11-06 20:48:34 +0000

    log access token

--- src/routes/api/if/+server.js
+++ src/routes/api/if/+server.js
@@ -1,52 +1,51 @@
-import { error } from '@sveltejs/kit'
-import { Client, Environment } from 'square'
-import { env } from '$env/dynamic/private'
+import { error } from '@sveltejs/kit';
+import { Client, Environment } from 'square';
+import { env } from '$env/dynamic/private';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({}) {
-  const client = new Client({
-    accessToken: env.SQUARE_ACCESS_TOKEN,
-    types: 'ITEM',
-  })
+	console.log(env.SQUARE_ACCESS_TOKEN);
+	const client = new Client({
+		accessToken: env.SQUARE_ACCESS_TOKEN,
+		types: 'ITEM'
+	});
 
-  const { catalogApi } = client
+	const { catalogApi } = client;
 
-  const response = await catalogApi.listCatalog()
+	const response = await catalogApi.listCatalog();
 
-  const items = response.result.objects.filter(
-    (objects) => objects.type === 'ITEM',
-  )
+	const items = response.result.objects.filter((objects) => objects.type === 'ITEM');
 
-  const results = items.map((item) => {
-    const {
-      id,
-      updatedAt: last_update,
-      itemData: {
-        name: title,
-        description,
-        imageIds: [image_url],
-      },
-    } = item
-    return {
-      id,
-      title,
-      description,
-      image_url,
-      last_update,
-      blob: {
-        title,
-        description,
-        image_url,
-      },
-    }
-  })
+	const results = items.map((item) => {
+		const {
+			id,
+			updatedAt: last_update,
+			itemData: {
+				name: title,
+				description,
+				imageIds: [image_url]
+			}
+		} = item;
+		return {
+			id,
+			title,
+			description,
+			image_url,
+			last_update,
+			blob: {
+				title,
+				description,
+				image_url
+			}
+		};
+	});
 
-  const data = {
-    results_size: items.length,
-    results,
-  }
+	const data = {
+		results_size: items.length,
+		results
+	};
 
-  const string = JSON.stringify(data)
+	const string = JSON.stringify(data);
 
-  return new Response(string)
+	return new Response(string);
 }
